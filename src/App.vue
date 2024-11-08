f82f38f0be9a731a6b529a5df5d7fbf429e8841c
mieramensatu <sasakihaise985@gmail.com>
2024-11-09 05:25:59 +0700

    directive if

--- src/App.vue
+++ src/App.vue
@@ -1,55 +1,159 @@
 <script setup>
-import { ref } from "vue";
-const count = ref(1);
+import { ref } from 'vue';
 
-function increase() {
-  count.value++;
-};
-function decrease() {
-  count.value--;
-};
+const showForm = ref(false);
 </script>
 
 <template>
   <main>
-    <h1>Counter</h1>
-    <span id="value">{{ count }}</span>
-    <div>
-      <button @click="decrease" id="decrease">-</button>
-      <button @click="increase" id="increase">+</button>
+    <div @click="showForm = true" class="container">
+      <header>
+        <h1 class="header-title">memo</h1>
+        <button class="header-button">+</button>
+      </header>
+      <div class="card-container">
+        <div class="card">
+          <p class="card-content">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae
+            atque vero autem magnam minus doloribus soluta necessitatibus, eos,
+            molestias corporis, numquam iure. Iste, quasi repellat accusamus hic
+            perferendis maxime excepturi?
+          </p>
+          <p>12/12/12</p>
+        </div>
+        <div class="card">
+          <p class="card-content">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae
+            atque vero autem magnam minus doloribus soluta necessitatibus, eos,
+            molestias corporis, numquam iure. Iste, quasi repellat accusamus hic
+            perferendis maxime excepturi?
+          </p>
+          <p>12/12/12</p>
+        </div>
+        <div class="card">
+          <p class="card-content">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae
+            atque vero autem magnam minus doloribus soluta necessitatibus, eos,
+            molestias corporis, numquam iure. Iste, quasi repellat accusamus hic
+            perferendis maxime excepturi?
+          </p>
+          <p>12/12/12</p>
+        </div>
+      </div>
+    </div>
+    <div v-if="showForm" class="form-overlay">
+      <div class="form-modal">
+        <button @click="showForm = false" class="form-close-btn">&times;</button>
+        <textarea name="memo" id="memo" cols="30" rows="10"></textarea>
+        <button class="form-save-btn">save</button>
+      </div>
     </div>
   </main>
 </template>
 
 <style scoped>
 main {
-  text-align: center;
-  background-color: #333333;
-  padding: 20px;
-  border-radius: 20px;
+  height: 100vh;
+  width: 100vw;
+}
+.container {
+  max-width: 900px;
+  margin: 0 auto;
+}
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+header .header-title {
+  color: gray;
+  font-size: 3rem;
+  font-weight: bold;
+  margin-bottom: 3rem;
 }
 
-#value {
-  font-size: 50px;
-}
-
-button {
-  font-size: 16px;
-  margin: 0 5px;
-  border-radius: 4px;
+header .header-button {
   cursor: pointer;
-  border: none;
-  background-color: #555;
-  color: #fff;
+  border: transparent;
+  border-radius: 100%;
+  height: 50px;
+  width: 50px;
+  padding: 10px;
+  background-color: cornflowerblue;
+  color: white;
+}
+
+.card {
+  width: 225px;
+  height: 225px;
+  padding: 10px;
+  background-color: #ffa6c1;
+  margin-bottom: 20px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+}
+
+.card-container {
+  display: flex;
+  flex-wrap: wrap;
+  gap: 20px;
+}
+
+.form-overlay {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.4);
+  z-index: 10;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.form-modal {
+  width: 420px;
+  background-color: white;
+  border-radius: 10px;
+  padding: 20px;
+  position: relative;
+  display: flex;
+  flex-direction: column;
+}
+
+.form-save-btn {
   padding: 10px 20px;
-  transition: 0.3s ease-in-out;
+  font-size: 20px;
+  width: 100%;
+  background-color: #495a7d;
+  border: none;
+  border-radius: 5px;
+  color: white;
+  cursor: pointer;
+  margin-top: 20px;
+  box-shadow: 0 0 10px #495a7d;
+  font-weight: bold;
+  transition: all 0.2s ease;
+  outline: none;
+}
+.form-save-btn:hover {
+  background-color: #516393;
+  box-shadow: 0 0 10px #516393;
+  transform: scale(1.03);
+  transition: all 0.2s ease;
+  outline: none;
 }
 
-button:hover {
-  background-color: #777777;
-}
-
-button:active {
-  transform: scale(0.9);
+.form-close-btn{
+  position: absolute;
+  top: 0;
+  right: 0;
+  width: 30px;
+  height: 30px;
+  background-color: transparent;
+  border: none;
+  font-size: 25px;
+  cursor: pointer;
 }
 </style>
